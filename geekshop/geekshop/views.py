827686fcd44e326fb32bcade7440cb4fa6dcba11
Lina <linavi96@yandex.ru>
2021-10-12 10:45:43 +0300

    lesson 2

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
+import json
 
 
 def main(request):
-    return render(request, 'geekshop/index.html')
+    title = 'Магазин'
+
+    with open("geekshop/menu.json", "r") as read_file:
+        links_menu = json.load(read_file)
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
+
+    return render(request, 'geekshop/index.html', context=context)
 
 
 def contacts(request):
-    return render(request, 'geekshop/contact.html')
+    title = 'Контакты'
+
+    with open("geekshop/menu.json", "r") as read_file:
+        links_menu = json.load(read_file)
+
+    with open("geekshop/locations.json", "r") as read_file:
+        locations = json.load(read_file)
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+        'locations': locations,
+    }
+
+    return render(request, 'geekshop/contact.html', context=context)
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,22 @@
+import json
+
 from django.shortcuts import render
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'Каталог'
+
+    with open("mainapp/catalog.json", "r") as read_file:
+        links_catalog = json.load(read_file)
+
+    with open("mainapp/menu.json", "r") as read_file:
+        links_menu = json.load(read_file)
+
+
+    context = {
+        'title': title,
+        'links_catalog': links_catalog,
+        'links_menu': links_menu,
+    }
+
+    return render(request, 'mainapp/products.html', context=context)
