e804df7db5ff272c431f96bd31c43e9188605ab5
Lina <linavi96@yandex.ru>
2021-11-28 17:09:35 +0300

    lesson_fin

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -100,9 +100,13 @@ LOGIN_ERROR_URL = '/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgresql',
     }
 }
 
