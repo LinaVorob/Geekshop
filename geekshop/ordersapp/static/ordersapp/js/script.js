ac91ba3d82b2a8999d7e017915c60c3c44a0d1ca
Lina <linavi96@yandex.ru>
2021-11-28 15:49:44 +0300

    lesson_2_5

--- geekshop/ordersapp/static/ordersapp/js/script.js
+++ geekshop/ordersapp/static/ordersapp/js/script.js
@@ -8,7 +8,7 @@ window.onload = function () {
     var order_total_price = parseFloat($('.order_total_cost').text().replace(',', '.')) || 0;
 
     for (var i=0; i < total_forms; i++) {
-        _quantity = parseInt($('input[name="orderitems-' + i + '-quantity]').val());
+        _quantity = parseInt($('input[name=orderitems-' + i + '-quantity]').val());
         _price = parseFloat($('.orderitems-' + i + '-price').text().replace(',', '.'));
 
         quantity_arr[i] = _quantity;
@@ -18,18 +18,22 @@ window.onload = function () {
             price_arr[i] = 0;
         }
 
-        $('.order_form').on('click', 'input[type=number]', function () {
+        $('.order_form').on('click', 'input[type="number"]', function () {
             var target = event.target;
             orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-quantity', ''));
+            console.log(price_arr[orderitem_num])
             if (price_arr[orderitem_num]) {
+                console.log(price_arr[orderitem_num])
                 orderitem_quantity = parseInt(target.value);
                 delta_quantity = orderitem_quantity - quantity_arr[orderitem_num];
                 quantity_arr[orderitem_num] = orderitem_quantity;
                 orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
             }
+            console.log('gjckr')
         });
 
-        $('.order_form').on('click', 'input[type=checkbox]', function () {
+        $('.order_form').on('click', 'input=[type="checkbox"]', function () {
+            console.log(event.target)
             var target = event.target;
             orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-quantity', ''));
             if(target.checked) {
@@ -46,6 +50,8 @@ window.onload = function () {
             order_total_price = Number((order_total_price + delta_cost).toFixed(2));
 
             order_total_quantity = order_total_quantity + delta_quantity;
+            console.log(order_total_quantity)
+
 
             $('.order_total_quantity').html(order_total_quantity.toString());
             $('.order_total_cost').html(order_total_price.toString() + ',00');
@@ -65,4 +71,52 @@ window.onload = function () {
         prefix: 'orderitems',
         remove: deleteOrderItem
     });
+
+    if (!order_total_quantity) {
+        orderSummaryRecalc();
+    }
+
+    function orderSummaryRecalc (){
+        order_total_quantity = 0;
+        order_total_price = 0;
+
+        for (let i=0; i<total_forms; i++){
+            order_total_quantity += quantity_arr[i];
+            order_total_price += price_arr[i] * quantity_arr[i];
+        }
+
+        $('.order_total_quantity').html(order_total_quantity.toString());
+        $('.order_total_cost').html(Number(order_total_price.toFixed(2).toString));
+    }
+
+    $('.order_form select').change(function (){
+        let target = event.target;
+        orderitem_num = parseInt(target.name.match(/\d+/))
+        let orderitem_product_pk = target.options[target.selectedIndex].value
+
+        if (orderitem_product_pk){
+            $.ajax({
+                url:`/orders/product/${orderitem_product_pk}/price/`,
+                success: function(data){
+                    if (data.price) {
+                        price_arr[orderitem_num] = parseFloat(data.price)
+                        if (isNaN(quantity_arr[orderitem_num])){
+                            quantity_arr[orderitem_num] = 0;
+                        }
+                        let price_html = '<span>' + data.price.toString().replace('.', ',') + '</span> руб.'
+                        let current_tr = $('.order_form table').find('tr:eq(' + (orderitem_num + 1) + ')');
+                        current_tr.find('td:eq(2)').html(price_html);
+
+                        if (isNaN(current_tr.find('input[type="number"]').val())){
+                            current_tr.find('input[type="number"]').val(0);
+                        }
+
+                        orderSummaryRecalc();
+                    }
+                }
+            });
+        }
+    });
+
+
 }
