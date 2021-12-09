f0e05f93e65c57d82391717f5a4bbff5efa5e4bf
LinVor <linavi96@yandex.ru>
2021-12-09 20:51:55 +0300

    testing (2)

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -5,7 +5,85 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 
 from mainapp.models import ProductCategory, Product
+from django.conf import settings
+from django.core.cache import cache
 
+
+def get_links_menu():
+    if settings.LOW_CACHE:
+        key = 'links_menu'
+        links_menu = cache.get(key)
+        if links_menu is None:
+            links_menu = ProductCategory.objects.filter(is_active=True)
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.filter(is_active=True)
+
+
+def get_category(pk):
+    if settings.LOW_CACHE:
+        key = f'category_{pk}'
+        category = cache.get(key)
+        if category is None:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            cache.set(key, category)
+        return category
+    else:
+        return get_object_or_404(ProductCategory, pk=pk)
+
+
+def get_products():
+    if settings.LOW_CACHE:
+        key = 'products'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.select_related('category').filter(is_active=True,
+                                                                         quantity__gte=1).select_related('category')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.select_related('category').filter(is_active=True,
+                                                                 quantity__gte=1).select_related('category')
+
+
+def get_product(pk):
+    if settings.LOW_CACHE:
+        key = f'product_{pk}'
+        product = cache.get(key)
+        if product is None:
+            product = get_object_or_404(Product, pk=pk)
+            cache.set(key, product)
+        return product
+    else:
+        return get_object_or_404(Product, pk=pk)
+
+
+def get_products_orederd_by_price():
+    if settings.LOW_CACHE:
+        key = 'products_orederd_by_price'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True, category__is_active=True, quantity__gte=1).order_by(
+                'price')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True, category__is_active=True, quantity__gte=1).order_by('price')
+
+
+def get_products_in_category_orederd_by_price(pk):
+    if settings.LOW_CACHE:
+        key = f'products_in_category_orederd_by_price_{pk}'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True,
+                                              quantity__gte=1).order_by('price')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True,
+                                      quantity__gte=1).order_by('price')
 
 
 def get_hot_product():
@@ -21,11 +99,8 @@ def get_same_products(hot_product):
 def products(request, pk=None, page=1):
     title = 'Каталог'
 
-    links_catalog = []
-    products = Product.objects.select_related('category').filter(is_active=True, quantity__gte=1).order_by('price')
-    for product in products:
-        if product.category not in links_catalog:
-            links_catalog += ProductCategory.objects.filter(name=product.category)
+    links_catalog = get_links_menu()
+    products = get_products_orederd_by_price()
 
     with open("mainapp/menu.json", "r", encoding='utf-8') as read_file:
         links_menu = json.load(read_file)
@@ -33,10 +108,10 @@ def products(request, pk=None, page=1):
     if pk is not None:
         if pk == 0:
             category = {'pk': 0, 'name': 'все'}
-            products = Product.objects.filter(is_active=True, category__is_active=True, quantity__gte=1).order_by('price')
+            products = get_products_in_category_orederd_by_price(pk)
         else:
-            category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True, quantity__gte=1).order_by('price')
+            category = get_category(pk)
+            products = get_products_in_category_orederd_by_price(pk)
         paginator = Paginator(products, 2)
         try:
             products_paginator = paginator.page(page)
@@ -69,18 +144,14 @@ def products(request, pk=None, page=1):
 
 
 def product(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    links_catalog = []
-    products = Product.objects.filter(is_active=True).order_by('price')
-    for item in products:
-        if item.category not in links_catalog:
-            links_catalog += ProductCategory.objects.filter(name=item.category)
+    product = get_product(pk)
+
     with open("mainapp/menu.json", "r", encoding='utf-8') as read_file:
         links_menu = json.load(read_file)
     context = {
         'title': product.name,
         'links_menu': links_menu,
-        'links_catalog': links_catalog,
+        'links_catalog': get_links_menu(),
         'product': product,
         'same_products': get_same_products(product),
     }
